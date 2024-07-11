91a04c53f274231da5faa3054eceb9383ab350eb
vinay <vinaybv77716@gmail.com>
2024-07-11 19:58:59 +0530

    updated to install docker-compose

--- Task1/one.sh
+++ Task1/one.sh
@@ -19,8 +19,31 @@ sudo apt install apache2 -y
 
 echo "###########install Dockers##############"
 
-sudo apt install docker.io -y
-sudo apt install docker-compose -y
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+docker-compose --version
+
 sudo chown $USER  /var/run/docker.sock
 sudo chown  jenkins:jenkins /var/www/html/
 sudo chown  jenkins:jenkins /var/www/html
